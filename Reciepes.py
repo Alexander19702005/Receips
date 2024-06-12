75919dbb8c9bf3e6b567d295891ee07001e25477
Alexander19702005 <asapogov@mail.ru>
2024-06-12 10:16:43 +0500

    Add files via upload
    Changes

---
+++ Reciepes.py
@@ -0,0 +1,24 @@
+#Задание 1
+import os
+
+with open('recipes.txt', encoding='utf-8') as file:
+    cook_book = {}
+    for k in file:
+        recepie_name = k.strip()
+        print(recepie_name)
+        ingredients_count = file.readline()
+        #print(ingredients_count)
+
+        for p in range(int(ingredients_count)):
+            ingredients = []
+            recepie = file.readline().strip().split(' | ')
+            product, quantity, weight = recepie
+            ingredients.append({'ingredient_name': product, 'quantity': quantity, 'measure': weight})
+        file.readline()
+    cook_book[recepie_name] = ingredients
+    print(cook_book)
+    #print(ingredients)
+
+
+
+
