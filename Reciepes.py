e154ce3ae9fed357bf5a2b9a49f9e46ed8b18906
Alexander19702005 <asapogov@mail.ru>
2024-06-13 11:52:42 +0500

    Next commit

--- Reciepes.py
+++ Reciepes.py
@@ -3,20 +3,21 @@ import os
 
 with open('recipes.txt', encoding='utf-8') as file:
     cook_book = {}
+    ingredients = []
     for k in file:
         recepie_name = k.strip()
-        print(recepie_name)
+        #print(recepie_name)
         ingredients_count = file.readline()
         #print(ingredients_count)
 
         for p in range(int(ingredients_count)):
-            ingredients = []
             recepie = file.readline().strip().split(' | ')
             product, quantity, weight = recepie
             ingredients.append({'ingredient_name': product, 'quantity': quantity, 'measure': weight})
         file.readline()
-    cook_book[recepie_name] = ingredients
+    cook_book[recepie_name]=ingredients
     print(cook_book)
+    #print(t)
     #print(ingredients)
 
 
